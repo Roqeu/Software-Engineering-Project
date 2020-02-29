d8c7e9257f4ac19d2b72fb1bbf59a236e78641a1
John Desbois <jdesbois@me.com>
2020-02-29 15:36:36 +0000

    Completed most of the methods, near to work through some of the tweaks with controller

--- Main Game/MainLogic/View.java
+++ Main Game/MainLogic/View.java
@@ -15,15 +15,7 @@ public class View {
         System.out.println("1. Course Director \n2. Administrator\n3. PTT Director");
         exit();
     }
-
-
-    //Course director methods
-    public void courseDirectorOptions() {
-        System.out.println("Welcome Course Director, please select from the below options:");
-        System.out.println("1. View Courses\n2. Create Course");
-        exit();
-    }
-    public void listCourses(ArrayList<Course> courses) {
+    public void displayCourses(ArrayList<Course> courses) {
         //iterate over course list and print out each class, then a quit item at the end
         int selectNum = 1;
         for (Course course : courses) {
@@ -32,8 +24,20 @@ public class View {
         }
         exit();
     }
+
+    //Course director methods
+    public void courseDirectorOptions() {
+        System.out.println("Welcome Course Director, please select from the below options:");
+        System.out.println("1. View Courses\n2. Create Course");
+        exit();
+    }
+
     public void createCourse() {
         System.out.println("Please enter the course name, followed by the required staff on a new line");
+    }
+
+    public void askRequirement() {
+        System.out.println("Please enter the number of required staff for selected course: ");
     }
 
     //Admin methods
@@ -42,21 +46,35 @@ public class View {
         System.out.println("1. View Staff \n2. View Course Requirements \n3. Create Staff \n4. View Untrained Staff");
         exit();
     }
-    public void displayStaffList(ArrayList<Staff> staffList) {
+    public void displayStaff(ArrayList<Staff> staffList) {
         for (Staff staff: staffList) {
             System.out.println(staff);
         }
         exit();
     }
-    public void displayTeachingRequirments(ArrayList<Course> requirements) {
-        int selectNum = 1;
-        for(Course course : requirements) {
-            System.out.println(selectNum + ". " + course.getName() + ", Required Staff: " + course.getRequirements() + ", Assigned Staff: " +course.getAssignedStaff());
-            selectNum++;
+    public void displayCourse(Course course) {
+        System.out.println("Course: " + course.getName());
+        System.out.println("Required Staff: " + course.getRequirements());
+        System.out.println("Assigned Staff: " + course.getAssignedStaff());
+    }
+    public void displayUntrainedStaff(ArrayList<Staff> staffList) {
+        for (Staff staff : staffList) {
+            System.out.println(staff);
         }
         exit();
     }
+    public void createStaff() {
+        System.out.println("Please enter the name of the member of staff below: ");
+    }
 
+    // public void displayCourseRequirments(ArrayList<Course> requirements) {
+    //     int selectNum = 1;
+    //     for(Course course : requirements) {
+    //         System.out.println(selectNum + ". " + course.getName() + ", Required Staff: " + course.getRequirements() + ", Assigned Staff: " +course.getAssignedStaff());
+    //         selectNum++;
+    //     }
+    //     exit();
+    // }
 
     //PTT Director methods
     public void pttDirectorOptions() {
