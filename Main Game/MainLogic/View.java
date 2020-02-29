3b18c22a5cc1cdb77fb4b93649584c9a3c3cbe95
John Desbois <jdesbois@me.com>
2020-02-29 10:16:52 +0000

    List courses method completed

--- Main Game/MainLogic/View.java
+++ Main Game/MainLogic/View.java
@@ -2,34 +2,45 @@ package MainLogic;
 
 import java.util.ArrayList;
 
-public class View {
 
+
+public class View {
+    /**
+     * Contstructor
+     */
     public View ()  {
 
     }
 
     public void selectUser() {
         System.out.println("Please select user:");
-        System.out.println("1. Course Director \n2. Administrator\n3. PTT Director\n0. Quit");
+        System.out.println("1. Course Director \n2. Administrator\n3. PTT Director\n0. Exit");
     }
 
-    public void courseDirectorOption() {
+    public void courseDirectorOptions() {
         System.out.println("Welcome Course Director, please select from the below options:");
-        System.out.println("1. View Courses\n2. Create Course");
+        System.out.println("1. View Courses\n2. Create Course \n0. Exit");
     }
 
     public void adminOptions() {
         System.out.println("Welcome Administrator, please select from the below options:");
-        System.out.println("1. View Staff \n2. View Course Requirements \n3. Create Staff \n4. View Untrained Staff");
+        System.out.println("1. View Staff \n2. View Course Requirements \n3. Create Staff \n4. View Untrained Staff \n0. Exit");
     }
 
     public void pttDirectorOptions() {
         System.out.println("Welcome PTT Director, please select from the below options:");
-        System.out.println("1. View Courses for Approval \n2. View Approved Courses");
+        System.out.println("1. View Courses for Approval \n2. View Approved Courses \n0. Exit");
     }
 
-    public void viewCourses(ArrayList<Course> courses) {
-        
+    public void listCourses(ArrayList<Course> courses) {
+        //iterate over course list and print out each class, then a quit item at the end
+        int selectNum = 1;
+        for (Course course : courses) {
+            System.out.println(selectNum+". "+ course.getName());
+            selectNum++;
+        }
+        System.out.println("0. Exit");
     }
 
+    
 }
