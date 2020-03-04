41c9b7d82b6091a7b14ce80ba20e5979e8a4f4a4
rdinneen <rebecca.dinneen@gmail.com>
2020-03-04 14:07:59 +0000

    javadoc comments added

--- Main Game/Course.java
+++ Main Game/Course.java
@@ -1,14 +1,37 @@
-
+package MainLogic;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class for course creation/methods
+ * @author Rebecca
+ * 
+ * Public methods;
+ * public boolean checkFull
+ *public void addStaff (Staff staff)
+ *public void removeStaff (Staff staff)
+ *public boolean changeRequirements (int newReq)
+ *public int getID
+ *public String getName
+ *private List<Integer> getStaffID
+ *private int getRequiredStaff
+ *private int getAssignedStaff
+ *public void setRequirements(int requirements)
+ *public boolean isApproved
+ *public void setApproved(boolean approved)
+ */
+
 public class Course {
-	private int ID;
-	private String name;
-	private boolean approved;
-	private int requiredStaff;
-	private int assignedStaff;
-	private List<Integer> staffID;
+	private int ID; //course ID
+	private String name; //course name
+	private boolean approved;//course approved
+	private int requiredStaff; //staff required for course 
+	private int assignedStaff;//staff assigned to course
+	private List<Integer> staffID;//List of staff ID's
+	
+	/**
+	 * Constructor for courses which passed to courseToFile method in writeToFile class
+	 */
 	
 	public Course(int ID, String name, boolean approved, int requiredStaff, int assignedStaff, List<Integer> staffID) {
 		this.ID = ID;
@@ -19,14 +42,23 @@ public class Course {
 		this.staffID = staffID;
 	}
 	
-	public Course(int ID, String name, int requiredStaff) {
+	/**
+	 * Constructor
+	 */
+	
+	public Course(int ID, String name, int requiredStaff) { 
 		this.ID = ID;
 		this.name = name;
 		this.requiredStaff = requiredStaff;
 		this.assignedStaff = 0;
-		this.staffID = new ArrayList<Integer>();
+		this.staffID = new ArrayList<Integer>(); //ArrayList of staffIDs created when Course object created
 		
 	}
+	
+	/**
+	 * Method to find check if course full
+	 * @return true if course full, false if not
+	 */
 	
 	public boolean checkFull() {
 		if (this.requiredStaff == this.assignedStaff) {
@@ -36,10 +68,21 @@ public class Course {
 		return false;	
 	}
 	
+	/**
+	 * Method to add staff to course
+	 * @param staff
+	 */
+	
 	public void addStaff(Staff staff) {
 		this.staffID.add(staff.getID());
 		
 	}
+	
+	/**
+	 * Method to remove staff from course
+	 * @param staff
+	 *If staff 
+	 */
 	
 	public void removeStaff(Staff staff) {
 		
@@ -53,7 +96,13 @@ public class Course {
 		
 	}
 	
-	public boolean changeRequirements(int newReq ) {
+	/**
+	 * Method to change required staff if new requirements added to course
+	 * @param newReq
+	 * @return false if assigned staff greater than new requirements, else set required staff to new requirements and return true
+	 */
+	
+	public boolean changeRequirements(int newReq) {
 			
 			if (this.assignedStaff > newReq ) {
 				
@@ -63,29 +112,58 @@ public class Course {
 				this.requiredStaff = newReq;
 				
 				return true;
-			}
-			
+			}	
 	}
+	/**
+	 * Method to get course ID
+	 * @return course ID
+	 */
 	
 	public int getID() {
 		return ID;
 	}
 	
+	/**
+	 * Method to get course name
+	 * @return course name
+	 */
+	
 	public String getName() {
 		return name;
 	}
 	
-	public List<Integer> getStaffID() {
+	/**
+	 * Method to get list of staff ID's
+	 * @return list of staff ID's
+	 */
+	
+	private List<Integer> getStaffID() {
 		return staffID;
 	}
 	
-	public int getRequiredStaff() {
+	/**
+	 * Method to get required staff
+	 * @return required staff
+	 */
+	
+	private int getRequiredStaff() {
 		return requiredStaff;
 	}
 	
-	public int getAssignedStaff() {
+	/**
+	 * Method to get assigned staff
+	 * @return assigned staff
+	 */
+	
+	private int getAssignedStaff() {
 		return assignedStaff;
 	}
+	
+	/**
+	 * Method to set requirements
+	 * @param requirements
+	 * @return requirements
+	 */
 
 	
 	public void setRequirements(int requirements) {
@@ -93,15 +171,30 @@ public class Course {
 		
 	}
 	
+	/**
+	 * Method to get requirements
+	 * @return required staff
+	 */
+	
 	public int getRequirements() {
 		return this.requiredStaff;
 		
 	}
 	
+	/**
+	 * Method to approve course
+	 * @return approved (true/false)
+	 */
 	public boolean isApproved() {
 		return approved;
 		
 	}
+	
+
+	/**
+	 * Method to set approved status
+	 * @param approved
+	 */
 	
 	public void setApproved(boolean approved) {
 		this.approved = approved;
--- Main Game/Staff.java
+++ Main Game/Staff.java
@@ -1,12 +1,30 @@
 package MainLogic;
-import java.util.ArrayList;
+
+/**
+ * Class 
+ * @author Rebecca
+ *
+ *public int getID
+ *public void setID(int iD)
+ *public String getName
+ *public void setName(String name)
+ *public boolean isAvailable
+ *public void setAvailable(boolean available)
+ *public boolean isTrained
+ *public void setTrained(boolean trained) 
+ *public String toString
+ */
 
 public class Staff {
 	
-	private int ID;
-	private String name;
-	private boolean available;
-	private boolean trained;
+	private int ID; // staff ID
+	private String name; //staff name
+	private boolean available; //staff available
+	private boolean trained;//staff trained
+	
+	/**
+	 * Constructor for staff 
+	 */
 	
 	public Staff(String name, int ID) {
 		this.name = name;
@@ -15,44 +33,93 @@ public class Staff {
 		this.trained = false;
 	}
 	
+
+	/**
+	 * Constructor for staff which passed to staffToFile method in writeToFile class
+	 */
+	
 	public Staff(String name,int ID, boolean available, boolean trained) {
 		this.name = name;
 		this.ID = ID;
 		this.available = available;
 		this.trained = trained;
 	}
+	
+	/**
+	 * Method to get staff ID
+	 * @return staff ID
+	 */
 
 	public int getID() {
 		return ID;
 	}
+	
+	/**
+	 * Method to set ID
+	 * @param iD
+	 */
 
 	public void setID(int iD) {
 		ID = iD;
 	}
+	
+	/**
+	 * Method to get staff name
+	 * @return staff name
+	 */
 
 	public String getName() {
 		return name;
 	}
+	
+	/**
+	 * Method to set staff name
+	 * @param name
+	 */
 
 	public void setName(String name) {
 		this.name = name;
 	}
+	/**
+	 * Method to set if staff available
+	 * @return true if available, false if not
+	 */
 
 	public boolean isAvailable() {
 		return available;
 	}
+	
+	/**
+	 * Method to set staff availability
+	 * @param available
+	 */
 
 	public void setAvailable(boolean available) {
 		this.available = available;
 	}
+	
+	/**
+	 * Method to check whether staff trained
+	 * @return trained true, untrained false
+	 */
 
 	public boolean isTrained() {
 		return trained;
 	}
+	
+	/**
+	 * Method to set whether staff trained
+	 * @param trained
+	 */
 
 	public void setTrained(boolean trained) {
 		this.trained = trained;
 	}
+	
+	/**
+	 * Method to format printing of staff objects
+	 * @return string formatted staff objects
+	 */
 
 	public String toString() {
 		String output = "";
